6fa05f73a1d924b624bd60334c2e3e835bee94a9
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2020-10-04 16:53:11 +0900

    update

--- allennlp_eraser/training/metrics/results.py
+++ allennlp_eraser/training/metrics/results.py
@@ -1,10 +1,34 @@
-from dataclasses import dataclass
-from typing import Any
+from dataclasses import dataclass, field
+from typing import Any, List
+
+
+@dataclass
+class SpanResult(object):
+    start_token: str
+    end_token: str
 
 
 @dataclass
 class RationalResult(object):
     docid: str
-    hard_rational_predictions: Any
+    hard_rational_predictions: List[SpanResult] = field(default_factory=list)
     soft_rationale_predictions: Any
     soft_sentence_predictions: Any
+
+
+@dataclass
+class ThresholdedScore(object):
+    threshold: float
+    comprehensiveness_classification_scores: Any
+    sufficiency_classification_scores: Any
+
+
+@dataclass
+class InstanceResult(object):
+    annotation_id: str
+    rationales: List[RationalResult]
+    classification: str = ""
+    classification_scores: dict
+    comprehensiveness_classification_scores: dict
+    sufficiency_classification_scores: dict
+    thresholded_scores: List[ThresholdedScore]
